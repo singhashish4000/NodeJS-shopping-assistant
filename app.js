71725dd5d1509100a1f9cbaf86cff921764ec9d3
Ashish Singh <singhashish4000@gmail.com>
2020-07-26 08:27:52 +0530

    dynanmic port

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(app.port, err => {
+app.listen(process.env.PORT || '3000', err => {
   console.log(`Server running on ${config.port}`)
 });
 
